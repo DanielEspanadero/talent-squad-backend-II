9f751c670a5af4f3cf6c7f3cd8e0286d03e9e85c
Daniel Españadero <despanadero@mytic.es>
2022-09-08 09:00:10 +0200

    First commit

---
+++ src/main/java/com/nuwe/backend/TalentSquadBackendIiApplication.java
@@ -0,0 +1,13 @@
+package com.nuwe.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentSquadBackendIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentSquadBackendIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuwe/backend/TalentSquadBackendIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuwe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentSquadBackendIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
